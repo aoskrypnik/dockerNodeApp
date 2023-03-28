343baee1ff480970431d6c681809b926b23c0c20
Andrew <47368110+aoskrypnik@users.noreply.github.com>
2023-03-28 21:12:55 +0200

    Test pipeline

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Andrii Skrypnik!!!');
+  res.send('Andrii Skrypnik!!! Test');
 });
 
 app.listen(PORT, HOST, () => {
